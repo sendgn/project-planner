c2b94c7c4aeb69debd5b0b91bb333ee40ccbc5ab
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-24 22:09:50 +0300

    Change font to Open Sans

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&display=swap');
+
 body {
   background: #f2f2f2;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   max-width: 600px;
   margin: 0 auto;
-  color: #555;
-  letter-spacing: 0.2px;
+  color: #666;
 }
 </style>
